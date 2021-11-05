f7e76baca928aba1962f6eb7af64113ec84f1590
Leo Dias <leodiascarvalhosr@gmail.com>
2021-11-04 22:08:38 -0300

    Acrescentando componentes estaticos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import NavBar from "components/NavBar";
+import Footer from "components/footer";
+import DateTable from "components/DateTable";
+
 function App() {
   return (
-    <div>
+    <>
+    <NavBar/>
+    <div className="container">
       <h1 className="text-primary">Olá mundo</h1>
+      <DateTable/>
     </div>
+    <Footer/>
+    </>
   );
 }
 
